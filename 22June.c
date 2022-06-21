1c9cc8ece040dfc8bf4d9264ee359aad6fa8a310
= <=>
2022-06-22 00:25:21 +0530

    yo again

---
+++ 22June.c
@@ -0,0 +1,135 @@
+#include<stdio.h>
+
+int Available[10], Allocation[10][10], Max[10][10], Need[10][10];
+int n_process, n_resources;
+int Finish[10], Work[10];
+// banker's algorithm
+
+int isSafe() {
+    int no_finished=0, last=0;
+    
+    for(int i=0; i<n_process; i++){
+        Finish[i] = 0;
+    }
+    for(int i=0; i<n_resources; i++){
+        Work[i] = Available[i];
+    }
+
+    do{
+        last = no_finished;
+        for(int i=0; i<n_process; i++){
+            if(Finish[i] == 0){
+                int flag = 1;
+                for(int j=0; j<n_resources; j++){
+                    if(Work[j] < Need[i][j]){
+                        flag = 0;
+                        printf("Process %d has to wait...\n", i);
+                        break;
+                    }
+                }
+                if(flag == 1){
+                    for(int j=0; j<n_resources; j++){
+                        Work[j] += Allocation[i][j];
+                    }
+                    printf("Proces %d completed execution...\n", i);
+                    Finish[i] = 1;
+                    no_finished++;
+                }
+            }
+        }
+    } while(no_finished!=last);
+    // if(no_finished==n_process)
+    return no_finished==n_process; 
+}
+
+void request(){
+    int req_process, Request[10];
+    printf("Enter the process with request: ");
+    scanf("%d",&req_process);
+    printf("Enter the request: ");
+    for(int i=0; i<n_resources; i++){
+        scanf("%d", &Request[i]);
+    }
+
+    for(int i=0; i<n_resources; i++)
+        Work[i] = Available[i];
+
+    int flag = 0;
+    // condition 1: check whether vaild request
+    for(int i=0; i<n_resources; i++)
+        if(Request[i]>Need[req_process][i]){
+            break;
+            flag=1;
+        }
+    if(flag){
+        printf("The request is invalid.");
+        return;
+    }
+    flag=0;
+    for(int i=0; i<n_resources; i++)
+        if(Request[i]>Work[i]){
+            break;
+            flag=1;
+        }
+    if(flag){
+        printf("The process has to wait due to lack of available free resrouces..");
+        return;
+    }
+
+    flag=0;
+    for(int i=0; i<n_resources; i++){
+        Work[i]=Work[i]-Request[i];
+        Allocation[req_process][i] = Allocation[req_process][i] + Request[i];
+        Need[req_process][i] = Need[req_process][i] + Request[i];
+    }
+
+    if(isSafe()){ 
+        printf("\nThe request can be satisfied immediately.");
+    }
+    else {
+        printf("The request cannot be satisfied immediately.");
+    }
+}
+
+void main(){
+    printf("Enter the no of processes (n): ");
+    scanf("%d", &n_process);
+    printf("Enter the no of types of resources (m): ");
+    scanf("%d", &n_resources);
+    printf("Enter the allocation matrix (n x m): \n");
+    for(int i=0; i<n_process; i++){
+        for(int j=0; j<n_resources; j++){
+            scanf("%d", &Allocation[i][j]);
+        }
+    }
+    printf("Enter the Max matrix (n x m): \n");
+    for(int i=0; i<n_process; i++){
+        for(int j=0; j<n_resources; j++){
+            scanf("%d", &Max[i][j]);
+        }
+    }
+
+    for(int i=0; i<n_process; i++){
+        for(int j=0; j<n_resources; j++){
+            Need[i][j] = Max[i][j] - Allocation[i][j];
+        }
+    }
+
+    printf("Enter the Available vector (m): \n");
+    for(int i=0; i<n_resources; i++){
+        scanf("%d", &Available[i]);
+    }
+
+    int safe = isSafe();
+
+    if(safe == 0){
+        printf("\n\nThe given snapshot is not safe");
+    }
+    else{
+        printf("\n\nThe given snapshot is in safe condition.");
+        request();
+    }
+
+}
+
+
